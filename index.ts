bab0cb1ca5065cd8310e212aa1a6fdeb4f98fcfe
nogahaskal <noghas12@gmail.com>
2020-03-27 13:51:16 +0300

    fix: fix error catching

--- index.ts
+++ index.ts
@@ -10,19 +10,20 @@ interface IOptions {
 }
 
 exports.getProtoFile = async (fileName: string, options: IOptions) => {
-    if (!options.githubBranch) options.githubBranch = 'master';
-    const dir = await createFolder('./proto');
-    const proto = await axios.get(`https://raw.githubusercontent.com/${options.repositoryOwner}/${options.repository}/${options.githubBranch}/${options.githubProtoPath}`, (err) => {
-        if (err) throw new Error(err);
-    });
-    const file = fs.promises.writeFile(`proto/${fileName}`, proto.data);
+    try {
+        if (!options.githubBranch) options.githubBranch = 'master';
+        await createFolder('./proto');
+        const proto = await axios.get(`https://raw.githubusercontent.com/${options.repositoryOwner}/${options.repository}/${options.githubBranch}/${options.githubProtoPath}`);
+        fs.promises.writeFile(`proto/${fileName}`, proto.data);
+    } catch (err) {
+        throw new Error(err);
+    }
 }
 
 exports.generateProto = async (protoPath: string) => {
     try {
-        const proto = await createFolder('./proto');
-        const generated = await createFolder('./proto/generated');
-        shell.exec(`grpc_tools_node_protoc --js_out=import_style=commonjs,binary:./proto/generated --grpc_out=./proto/generated -I ./proto ./${protoPath}`);
+        await createFolder('./proto');
+        await createFolder('./proto/generated');
         shell.exec(`grpc_tools_node_protoc \
         --plugin=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts \
         --ts_out=./proto/generated \
